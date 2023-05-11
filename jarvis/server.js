f27eb4a9f79c2e8914b853ba5cf90b129ef19e5d
Molik Kidd <mzkidd5@gmail.com>
2023-05-11 15:56:51 -0400

    add completions api call

--- jarvis/server.js
+++ jarvis/server.js
@@ -6,4 +6,24 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const API_KEY = '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';
+
+app.post('/completions', async (req,res) => {
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${API_KEY}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo"
+        })
+    }
+    try {
+        fetch('https://api.openai.com/v1/chat/completions', options)
+    } catch (error) {
+        console.error(error)
+    }
+})
 app.listen(PORT, () => console.log("Your server is running on PORT " + PORT))
