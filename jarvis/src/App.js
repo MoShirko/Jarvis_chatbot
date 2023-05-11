7f829dc793012e94ee93372ddaa8e310dc441d22
shirko <shir617@gmail.com>
2023-05-11 13:35:05 -0400

    styling added to input box

--- jarvis/src/App.js
+++ jarvis/src/App.js
@@ -3,12 +3,27 @@ const App = () => {
     <div className="app">
       <section className="side-bar">
         <button>+ New chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+          <li>bluh</li>
+        </ul>
         <nav>
           <p>Made by Mo & Shir</p>
         </nav>
       </section>
-      <section className="main"></section>
+      <section className="main">
+        <h1>Jarvis the chabot</h1>
+        <ul className="feed">
+        </ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input/>
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            Introducing Jarvis, an advanced AI-powered language model inspired by OpenAI's ChatGPT. Built on cutting-edge technology, this digital assistant is designed to engage users in meaningful, interactive conversations across a wide range of topics.
+          </p>
+        </div>
+      </section>
     </div>
   )
 }
