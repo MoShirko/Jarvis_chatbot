5c4ba4c0ac4dc6c42b6b506de6358f5870058c9c
shirko <shir617@gmail.com>
2023-05-11 12:37:48 -0400

    app.js frame added

---
+++ jarvis/src/App.js
@@ -0,0 +1,16 @@
+const App = () => {
+  return (
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Mo & Shir</p>
+        </nav>
+      </section>
+      <section className="main"></section>
+    </div>
+  )
+}
+
+export default App
