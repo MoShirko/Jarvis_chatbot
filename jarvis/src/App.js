0d3f95b44439b313f72167eab3506020cbc6fe1c
shirko <shir617@gmail.com>
2023-05-18 13:29:58 -0400

    add title role and content

--- jarvis/src/App.js
+++ jarvis/src/App.js
@@ -3,32 +3,65 @@ import { useState, useEffect } from "react";
 const App = () => {
   const [value, setValue] = useState(null);
   const [message, setMessage] = useState(null);
+  const [previousChats, setPreviousChats] = useState([]);
+  const [currentTitle, setCurrentTitle] = useState(null);
+  const createNewChat = () => {
+    setMessage(null)
+    setValue("")
+    setCurrentTitle(null)
+  }
 
   const getMessages = async () => {
     const options = {
       method: "POST",
       body: JSON.stringify({
-        message:value,
+        message: value,
       }),
-      headers : {
-        "Content-Type" : "application/json"
-      }
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
 
     try {
-      const response = await fetch("http://localhost:8000/completions", options);
-      const data = await response.json()
-      setMessage(data.choices[0].message)
+      const response = await fetch(
+        "http://localhost:8000/completions",
+        options
+      );
+      const data = await response.json();
+      setMessage(data.choices[0].message);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   };
 
-  console.log('value', value)
+  useEffect(() => {
+    console.log(currentTitle, value, message);
+    if(!currentTitle && value && message) {
+      setCurrentTitle(value)
+    }
+    if (currentTitle && value && message) {
+      setPreviousChats(prevChats => (
+        [...prevChats, 
+          {
+          title: currentTitle,
+          role: "user",
+          content: value
+          },
+          {
+          title: currentTitle,  
+          role: message.role,
+          content: message.content
+          }]
+      ))
+    }
+  }, [message, currentTitle]);
+
+  console.log(previousChats)
+
   return (
     <div className="app">
       <section className="side-bar">
-        <button>+ New chat</button>
+        <button onClick={createNewChat}>+ New chat</button>
         <ul className="history">
           <li>bluh</li>
         </ul>
@@ -37,11 +70,11 @@ const App = () => {
         </nav>
       </section>
       <section className="main">
-        <h1>Jarvis the chabot</h1>
+        {!currentTitle && <h1>Jarvis the chabot</h1>}
         <ul className="feed"></ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <input value={value} onChange={(e) => setValue(e.target.value)} />
             <div id="submit" onClick={getMessages}>
               ➢
             </div>
